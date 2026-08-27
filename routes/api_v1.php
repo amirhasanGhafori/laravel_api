b5da78ac598314edea37ceb72d90e8a34ab96f15
AmirhasanGhafori <amirhasanghafori.tabar@gmail.com>
2026-08-27 15:49:27 +0330

    add comment

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -24,4 +24,5 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('users', UserController::class)->except('update');
     Route::patch('users/{user}', [UserController::class, 'update']);
     Route::put('users/{users}', [UserController::class, 'replace']);
+    //group route manager
 });
